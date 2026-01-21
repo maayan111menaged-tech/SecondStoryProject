07c7d620d8fee69e2d895b221b5659314608e2aa
Maayan <maayan111menaged@gmail.com>
2026-01-21 11:10:37 +0200

    init eod 21/1 finished side bar no connection with the other pages yet.

--- app/src/main/java/com/example/secondstoryproject/screens/LandingActivity.java
+++ app/src/main/java/com/example/secondstoryproject/screens/LandingActivity.java
@@ -28,7 +28,7 @@ public class LandingActivity extends AppCompatActivity {
         });
 
         if(SharedPreferencesUtil.isUserLoggedIn(this)){
-            Intent intent = new Intent(LandingActivity.this, navpageActivity.class);
+            Intent intent = new Intent(LandingActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
             return;
--- app/src/main/java/com/example/secondstoryproject/screens/MainActivity.java
+++ app/src/main/java/com/example/secondstoryproject/screens/MainActivity.java
@@ -1,41 +1,86 @@
 package com.example.secondstoryproject.screens;
 
+import android.app.Notification;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.appcompat.graphics.drawable.DrawerArrowDrawable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.secondstoryproject.R;
 import com.example.secondstoryproject.models.User;
 import com.example.secondstoryproject.models.UserLevel;
 import com.example.secondstoryproject.utils.SharedPreferencesUtil;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     private static final String TAG = "MainActivity";
+    DrawerLayout drawerLayout;
     ProgressBar rateProgressBar;
     ImageView currentRateIcon,nextRateIcon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.nav_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.baseline_menu_24);
+        }
+
+        drawerLayout = findViewById(R.id.nav_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+
+        DrawerArrowDrawable arrowDrawable = toggle.getDrawerArrowDrawable();
+        arrowDrawable.setBarLength(80f);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(android.R.color.black));
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
 
         rateProgressBar = findViewById(R.id.rateProgressBar);
         currentRateIcon = findViewById(R.id.currentRateIcon);
@@ -79,6 +124,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // סגירת ה-Drawer אחרי בחירה
+        drawerLayout.closeDrawer(GravityCompat.START);
 
+        // TODO: חיבור לעמודים שונים בהתאם ל-item.getItemId()
+        return true;
+    }
 
+    @Override
+    public void onBackPressed() {
+        // אם ה-Drawer פתוח, סוגר אותו במקום לצאת
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
+
